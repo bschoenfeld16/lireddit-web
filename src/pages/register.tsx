2cf607c7c1567af29cbe00247670267e290ff232
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-11 17:12:50 -0600

    Creating useRegisterMutation hook and setting up the graphQL plugin

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,34 +4,18 @@ import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import {Box} from "@chakra-ui/layout";
 import {Button} from "@chakra-ui/button";
-import {useMutation} from "urql";
+import {useRegisterMutation} from "../generated/graphql";
 
 interface registerProps {
 }
 
-const REGISTER_MUT = `
-mutation Register($username: String!, $password: String!) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}
-`;
-
-
 const Register: React.FC<registerProps> = ({}) => {
-    const [, register] = useMutation(REGISTER_MUT);
+    const [, register] = useRegisterMutation();
     return (
         <Wrapper variant="small">
             <Formik initialValues={{username: "", password: ""}}
-                    onSubmit={values => {
-                        return register(values)
+                    onSubmit={async values => {
+                        const response = await register(values);
                     }}>
                 {({isSubmitting}) => (
                     <Form>
