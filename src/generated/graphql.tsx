b927b34eeddb1321141c0dc8c35ab4d81143895f
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-11 20:48:54 -0600

    navbar and determining if user is logged in using the me graphql query

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -141,6 +141,19 @@ export type RegisterMutation = (
 }
     );
 
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = (
+    { __typename?: 'Query' }
+    & {
+    me?: Maybe<(
+        { __typename?: 'User' }
+        & Pick<User, 'id' | 'username'>
+        )>
+}
+    );
+
 
 export const LoginDocument = gql`
     mutation Login($options: UsernamePasswordInput!) {
@@ -160,7 +173,6 @@ export const LoginDocument = gql`
 export function useLoginMutation() {
     return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
 }
-
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
         register(options: {username: $username, password: $password}) {
@@ -178,4 +190,17 @@ export const RegisterDocument = gql`
 
 export function useRegisterMutation() {
     return Urql.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument);
+}
+
+export const MeDocument = gql`
+    query Me {
+        me {
+            id
+            username
+        }
+    }
+`;
+
+export function useMeQuery(options: Omit<Urql.UseQueryArgs<MeQueryVariables>, 'query'> = {}) {
+    return Urql.useQuery<MeQuery>({query: MeDocument, ...options});
 }
