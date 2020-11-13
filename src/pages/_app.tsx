8dd370cec6bacf41eeba05e6cb7226584839c748
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-12 20:13:46 -0600

    logout functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import {ThemeProvider, CSSReset, ColorModeProvider} from '@chakra-ui/core'
 import theme from '../theme'
 import {createClient, dedupExchange, fetchExchange, Provider} from "urql";
 import {cacheExchange, Cache, QueryInput} from "@urql/exchange-graphcache";
-import {LoginMutation, MeDocument, MeQuery, RegisterMutation} from "../generated/graphql";
+import {LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation} from "../generated/graphql";
 
 function betterUpdateQuery<Result, Query>(
     cache: Cache,
@@ -22,6 +22,14 @@ const client = createClient({
     exchanges: [dedupExchange, cacheExchange({
         updates: {
             Mutation: {
+                logout: (_result, _args, cache, _info) => {
+                    betterUpdateQuery<LogoutMutation, MeQuery>(
+                        cache,
+                        {query: MeDocument},
+                        _result,
+                        () => ({me: null})
+                    )
+                },
                 login: (_result, _args, cache, _info) => {
                     betterUpdateQuery<LoginMutation, MeQuery>(
                         cache,
