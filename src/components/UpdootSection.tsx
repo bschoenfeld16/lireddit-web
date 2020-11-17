93cdcb17cd95115d8445fc9024e77df1335342cd
bschoenfeld16 <9916245+bschoenfeld16@users.noreply.github.com>
2020-11-16 22:12:42 -0600

    updoot and downdoot

--- src/components/UpdootSection.tsx
+++ src/components/UpdootSection.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Flex } from "@chakra-ui/layout";
 import { IconButton } from "@chakra-ui/button";
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
-import { PostSnippetFragment } from "../generated/graphql";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
 
 interface UpdootSectionProps {
     post: PostSnippetFragment;
 }
 
 const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+    const [loadingState, setLoadingState] = useState<
+        "updoot-loading" | "downdoot-loading" | "not-loading"
+    >("not-loading");
+    const [, vote] = useVoteMutation();
     return (
         <Flex
             direction="column"
@@ -16,13 +20,30 @@ const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
             justifyContent="center"
             mr={4}>
             <IconButton
-                onClick={() => console.log("yo")}
+                onClick={async () => {
+                    setLoadingState("updoot-loading");
+                    await vote({
+                        postId: post.id,
+                        value: 1,
+                    });
+                    setLoadingState("not-loading");
+                }}
+                isLoading={loadingState === "updoot-loading"}
                 aria-label="updoot post"
                 icon={<ChevronUpIcon />}
                 boxSize="24px"
             />
             {post.points}
             <IconButton
+                onClick={async () => {
+                    setLoadingState("downdoot-loading");
+                    await vote({
+                        postId: post.id,
+                        value: -1,
+                    });
+                    setLoadingState("not-loading");
+                }}
+                isLoading={loadingState === "downdoot-loading"}
                 aria-label="downdoot post"
                 icon={<ChevronDownIcon />}
                 boxSize="24px"
